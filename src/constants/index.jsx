43340eba54830873506965193ec60e0381a45cb5
harshcaturvedi29 <bludrgn29@gmail.com>
2024-05-31 09:25:06 +0500

    navbar initial

---
+++ src/constants/index.jsx
@@ -0,0 +1,8 @@
+export const navItems = [
+    { label: "Home", href: "#" },
+    { label: "ABout", href: "#" },
+    { label: "Services", href: "#" },
+    { label: "Portfolio", href: "#" },
+    { label: "Team", href: "#" },
+    { label: "Contact", href: "#" }
+  ];
